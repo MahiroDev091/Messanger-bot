a40b75af9c2e9c2243b6a187bf6f8462bb3f2f48
W3BD3V09728 <105326449+W3BD3V09728@users.noreply.github.com>
2023-01-12 07:58:19 +0800

    Create EventHandler.js

---
+++ src/EventHandler.js
@@ -0,0 +1,18 @@
+import config from '../database/config.js';
+
+import { GPTrespond } from './ChatGPT.js';
+
+async function EventHandler(event) {
+
+  let threadIDs = config.allowed_threads;
+
+  if (threadIDs.includes(event.threadID) && event.type == 'message') {
+
+    GPTrespond(event);
+
+  }
+
+}
+
+export default EventHandler;
+
